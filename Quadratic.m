36cd5a8377c4a75cdc3cf2f931550e80e4f0de08
FengYiLiao <robert147456@gmail.com>
2023-10-23 08:41:27 -0700

    update

--- Quadratic.m
+++
@@ -1,9 +0,0 @@
-clc;clear;close all;
-x1 = linspace(-1,1);
-x2 = x1;
-[X1,X2] = meshgrid(x1,x2);
-g0 = X1; 
-X1(X1<=0) = 0;
-obj = X1;
-surf(g0,X2,obj,'FaceColor','r','EdgeColor','none')
-xlim([-1,1])
